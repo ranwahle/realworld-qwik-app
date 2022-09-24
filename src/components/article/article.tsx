5ae9c7ce2661220ca9b1ecfa53a1ac8b768beae2
Ran <ran.wahle@gmail.com>
2022-09-24 13:30:14 +0300

    fix(home):rearrange css

--- src/components/article/article.tsx
+++ src/components/article/article.tsx
@@ -1,32 +1,14 @@
 import { component$ } from "@builder.io/qwik";
-import { formatDate } from "~/common/date-utils";
 import { ArticleData } from "~/model/article-data";
+import { ArticleMeta } from "~/routes/article/[articleName]/article-meta/article-meta";
 import { ArticleTagsList } from "../article-tags-list/article-tags-list";
 import "./article.css";
 
 export const Article = component$((props: { article: ArticleData }) => {
   const { article } = props;
-  const dateString = formatDate(article.createdAt);
   return (
     <div class="article-container">
-      <div class="article-meta">
-        <div class="article-info">
-          <img
-            src={article.author.imageUrl}
-            alt={article.author.username}
-          ></img>
-          <div>
-            <a class="author" href="">
-              {" "}
-              {article.author.username}
-            </a>
-            <span class="date">{dateString}</span>
-          </div>
-        </div>
-        <button class="article-favorites">
-          <i class="ion-heart"></i> {article.favoritesCount}
-        </button>
-      </div>
+      <ArticleMeta article={article}></ArticleMeta>
       <div class="article-title">
         <a href={`/article/${article.slug}`}> {article.title}</a>
       </div>
