c618dfd6f98908c7184364ff4e7ab4acbd9f87a8
Ran <ran.wahle@gmail.com>
2022-10-08 19:07:41 +0300

    fix(qwik-city):add node-fetch to rollup external

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
+    build: {
+      rollupOptions: {
+        external: ["node-fetch"],
+      },
+    },
     ssr: { target: "webworker", noExternal: true },
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
   };
