4e6720e42e469b92afa79058374220500317e4e1
Ran <ran.wahle@gmail.com>
2022-10-08 18:14:51 +0300

    fix(build): wip try change build script

--- src/routes/service-worker.ts
+++ src/routes/service-worker.ts
@@ -9,10 +9,10 @@
  */
 import { setupServiceWorker } from "@builder.io/qwik-city/service-worker";
 
-// setupServiceWorker();
-
-// addEventListener('install', () => self.skipWaiting());
-
-// addEventListener('activate', () => self.clients.claim());
+setupServiceWorker();
 
 declare const self: ServiceWorkerGlobalScope;
+
+addEventListener("install", () => self.skipWaiting());
+
+addEventListener("activate", () => self.clients.claim());
