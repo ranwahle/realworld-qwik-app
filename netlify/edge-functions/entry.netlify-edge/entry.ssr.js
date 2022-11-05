5fabb3117ccc19ec93a2e5835eee2eef52cd8339
Ran <ran.wahle@gmail.com>
2022-11-05 16:34:51 +0200

    fix(cludflare):add cloudflare pages

--- netlify/edge-functions/entry.netlify-edge/entry.ssr.js
+++ netlify/edge-functions/entry.netlify-edge/entry.ssr.js
@@ -1983,17 +1983,7 @@ const Ue = {
       {
         tag: "link",
         location: "head",
-        attributes: { rel: "stylesheet", href: "/build/q-9f98e854.css" },
-      },
-      {
-        tag: "link",
-        location: "head",
-        attributes: { rel: "stylesheet", href: "/build/q-d7bae8f5.css" },
-      },
-      {
-        tag: "link",
-        location: "head",
-        attributes: { rel: "stylesheet", href: "/build/q-20d67baa.css" },
+        attributes: { rel: "stylesheet", href: "/build/q-c6b87acc.css" },
       },
       {
         tag: "link",
@@ -2003,37 +1993,17 @@ const Ue = {
       {
         tag: "link",
         location: "head",
-        attributes: { rel: "stylesheet", href: "/build/q-15a6ad84.css" },
-      },
-      {
-        tag: "link",
-        location: "head",
-        attributes: { rel: "stylesheet", href: "/build/q-d20d8711.css" },
-      },
-      {
-        tag: "link",
-        location: "head",
-        attributes: { rel: "stylesheet", href: "/build/q-5bbd82c7.css" },
-      },
-      {
-        tag: "link",
-        location: "head",
-        attributes: { rel: "stylesheet", href: "/build/q-35b0a60c.css" },
-      },
-      {
-        tag: "link",
-        location: "head",
         attributes: { rel: "stylesheet", href: "/build/q-24868486.css" },
       },
       {
         tag: "link",
         location: "head",
-        attributes: { rel: "stylesheet", href: "/build/q-c6b87acc.css" },
+        attributes: { rel: "stylesheet", href: "/build/q-20d67baa.css" },
       },
       {
         tag: "link",
         location: "head",
-        attributes: { rel: "stylesheet", href: "/build/q-f372100c.css" },
+        attributes: { rel: "stylesheet", href: "/build/q-15a6ad84.css" },
       },
       {
         tag: "link",
@@ -2043,7 +2013,37 @@ const Ue = {
       {
         tag: "link",
         location: "head",
+        attributes: { rel: "stylesheet", href: "/build/q-9f98e854.css" },
+      },
+      {
+        tag: "link",
+        location: "head",
+        attributes: { rel: "stylesheet", href: "/build/q-35b0a60c.css" },
+      },
+      {
+        tag: "link",
+        location: "head",
+        attributes: { rel: "stylesheet", href: "/build/q-d7bae8f5.css" },
+      },
+      {
+        tag: "link",
+        location: "head",
+        attributes: { rel: "stylesheet", href: "/build/q-f372100c.css" },
+      },
+      {
+        tag: "link",
+        location: "head",
+        attributes: { rel: "stylesheet", href: "/build/q-d20d8711.css" },
+      },
+      {
+        tag: "link",
+        location: "head",
         attributes: { rel: "stylesheet", href: "/build/q-0b08a423.css" },
+      },
+      {
+        tag: "link",
+        location: "head",
+        attributes: { rel: "stylesheet", href: "/build/q-5bbd82c7.css" },
       },
     ],
     version: "1",
